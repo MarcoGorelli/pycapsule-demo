fec8309da38c5d299bf7ea30d9024ba589b4c9b6
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-01-06 17:25:28 +0000

    update for latest narwhals

--- run.py
+++ run.py
@@ -19,40 +19,25 @@ print("DuckDB result:", sum_i64_column(rel, column_name="a"))
 print()
 
 
-def agnostic_sum_i64_column_rust(df_native: IntoFrame, column_name: str) -> int:
-    df = nw.from_native(df_native)
-    schema = df.collect_schema()
-    if column_name not in schema:
-        msg = f"Column '{column_name}' not found, available columns are: {schema.names()}."
-        raise ValueError(msg)
-    if (dtype := schema[column_name]) != nw.Int64:
-        msg = f"Column '{column_name}' is of type {dtype}, expected Int64"
-        raise TypeError(msg)
-    return sum_i64_column_simple(df, column_name)
-
-
-print("***PyCapsule Interface + Narwhals solution***")
-print("Polars result:", agnostic_sum_i64_column_rust(df_pl, column_name="a"))
-print("pandas result:", agnostic_sum_i64_column_rust(df_pd, column_name="a"))
-print("PyArrow result:", agnostic_sum_i64_column_rust(df_pa, column_name="a"))
-rel = duckdb.sql("select * from data.csv")
-print("DuckDB result:", agnostic_sum_i64_column_rust(rel, column_name="a"))
-print()
-
-
 def agnostic_sum_i64_column_python(df_native: IntoFrame, column_name: str) -> int:
-    df = nw.from_native(df_native)
-    schema = df.collect_schema()
+    lf = nw.from_native(df_native).lazy()
+    schema = lf.collect_schema()
     if column_name not in schema:
         msg = f"Column '{column_name}' not found, available columns are: {schema.names()}."
         raise ValueError(msg)
     if (dtype := schema[column_name]) != nw.Int64:
         msg = f"Column '{column_name}' is of type {dtype}, expected Int64"
         raise TypeError(msg)
+    df = lf.collect()
     return df[column_name].sum()
+
 
 
 print("***Narwhals solution***")
 print("Polars result:", agnostic_sum_i64_column_python(df_pl, column_name="a"))
 print("pandas result:", agnostic_sum_i64_column_python(df_pd, column_name="a"))
 print("PyArrow result:", agnostic_sum_i64_column_python(df_pa, column_name="a"))
+# Needs re-executing due to https://github.com/duckdb/duckdb/discussions/15536.
+rel = duckdb.sql("select * from data.csv")
+print("DuckDB result:", agnostic_sum_i64_column_python(rel, column_name="a"))
+
